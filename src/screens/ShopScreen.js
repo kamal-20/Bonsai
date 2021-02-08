fcd86fdf6ff599523968f87aec3fecd218f280cf
Kamlendra Rathour <421kamlendra@gmail.com>
2021-02-08 19:50:12 +0530

    bottom tab auction icon

--- src/screens/ShopScreen.js
+++ src/screens/ShopScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import {Icon} from 'react-native-elements';
 
 const ShopScreen = () => {
     return (
@@ -8,5 +9,10 @@ const ShopScreen = () => {
         </View>
     )
 }
+
+ShopScreen.navigationOptions = {
+    title: 'Auctions',
+    tabBarIcon: <Icon type='evilicon' name='archive' size={30}  />
+  }
 
 export default ShopScreen;
