56cca4c58c6d8d55953d7741ea9664b8550df8e9
Kamlendra Rathour <421kamlendra@gmail.com>
2021-05-19 21:29:20 +0530

    update

--- src/screens/UpcomingAuctionScreen.js
+++ src/screens/UpcomingAuctionScreen.js
@@ -8,14 +8,53 @@ const UpcomingAuctionScreen = () => {
 
     return (
         <View>
-            <Text>UpcomingAuctionScreen</Text>
+            <FlatList
+                style={styles.list}
+                contentContainerStyle={styles.listContainer}
+                data = {upcomingAuctions}
+                horizontal={false}
+                keyExtractor= {(item)=>{
+                    return item.id;
+                }}
+                renderItem= {({item}) =>{
+                    return <TouchableOpacity
+                    >
+                    <Image  
+                        style={styles.image} 
+                        source={{uri:item.image}}
+                        PlaceholderContent= {<ActivityIndicator />}
+                        >
+                        <Text style={{fontSize:28,color:"white", margin: 10, marginLeft:10, fontFamily: "sans-serif-condensed", textAlign:"center"}} >{item.title}</Text>
+                    </Image>
+                    </TouchableOpacity> 
+                }}
+            />
 
         </View>
     )
 }
 
   const styles = StyleSheet.create({
-    
+    list: {
+        marginTop: 10,
+        marginHorizontal: 0,
+        paddingHorizontal: 0,
+        marginBottom: 10
+    },
+    listContainer:{
+        alignItems:'center',
+    },
+    image:{
+        marginHorizontal: 0,
+        alignItems: "center",
+        justifyContent: "center",
+        aspectRatio: 1,
+        width: '20%',
+        height: undefined ,
+        marginVertical: 10,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        borderRadius: 15,
+    }
 });
 
 export default UpcomingAuctionScreen;
