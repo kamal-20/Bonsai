47c2051eafebfc8d09888583bd0581e82edc611d
Kamlendra Rathour <421kamlendra@gmail.com>
2021-05-19 21:28:15 +0530

    update

--- src/screens/OngoingAuctionScreen.js
+++ src/screens/OngoingAuctionScreen.js
@@ -8,14 +8,54 @@ const OngoingAuctionScreen = () => {
 
     return (
         <View>
-            <Text>OngoingAuctionScreen</Text>
+
+            <FlatList
+                style={styles.list}
+                contentContainerStyle={styles.listContainer}
+                data = {ongoingAuctions}
+                horizontal={false}
+                keyExtractor= {(item)=>{
+                    return item.id;
+                }}
+                renderItem= {({item}) =>{
+                    return <TouchableOpacity
+                    >
+                    <Image  
+                        style={styles.image} 
+                        source={{uri:item.image}}
+                        PlaceholderContent= {<ActivityIndicator />}
+                        >
+                        <Text style={{fontSize:28,color:"white", margin: 10, marginLeft:10, fontFamily: "sans-serif-condensed", textAlign:"center"}} >{item.title}</Text>
+                    </Image>
+                    </TouchableOpacity> 
+                }}
+            />
 
         </View>
     )
 }
 
   const styles = StyleSheet.create({
-    
+    list: {
+        marginTop: 10,
+        marginHorizontal: 0,
+        paddingHorizontal: 0,
+        marginBottom: 10
+    },
+    listContainer:{
+        alignItems:'center',
+    },
+    image:{
+        marginHorizontal: 0,
+        alignItems: "center",
+        justifyContent: "center",
+        aspectRatio: 1,
+        width: '20%',
+        height: undefined ,
+        marginVertical: 10,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        borderRadius: 15,
+    }
 });
 
 export default OngoingAuctionScreen;
